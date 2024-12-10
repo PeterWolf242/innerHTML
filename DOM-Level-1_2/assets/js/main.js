999d208b3d672b5ac2e7f68256f44154d4bd05ee
[PeterWolf242] <[peter.ebm@web.de]>
2024-12-10 15:57:53 +0100

    innerHTML

---
+++ DOM-Level-1_2/assets/js/main.js
@@ -0,0 +1,10 @@
+const infoText = document.getElementById("info");
+
+infoText.innerHTML = "<h1>Hello World</h1>";
+infoText.innerHTML += "<h2>How are you ?</h2>"
+
+const Container = document.getElementById("container");
+
+Container.innerHTML = "<p>start of the Element</p>";
+
+document.write("end of the element.");
---
+++ DOM-Level-1_3/assets/js/main.js
@@ -0,0 +1,7 @@
+const galery = document.getElementById("gallery");
+
+galery.innerHTML = "<figure><img src='/assets/img/mongolischerwolf.jpg'></figure>";
+
+galery.innerHTML += "<figure><img src='/assets/img/mongolischerwolf.jpg'></figure>";
+
+galery.innerHTML += "<figure><img src='/assets/img/mongolischerwolf.jpg'></figure>";
